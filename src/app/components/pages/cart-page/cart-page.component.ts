d7609861e9217752c9bea2d07d0aba4c661503d3
Gustavo <srto.mendes@gmail.com>
2024-02-23 16:18:49 -0300

    update cart

--- src/app/components/pages/cart-page/cart-page.component.ts
+++ src/app/components/pages/cart-page/cart-page.component.ts
@@ -34,7 +34,7 @@ export class CartPageComponent implements OnInit {
     // Itera sobre a matriz de itens no objeto cart
     this.cart.items.forEach((item, index) => {
       // Adiciona o nome do item à string
-      itemsString += `${item.food.name} ☁️\n`;
+      itemsString += `☁️ ${item.food.name} \n`;
 
       // Adiciona uma vírgula e espaço após o nome do item, exceto para o último item
       if (index < this.cart.items.length - 1) {
